6ae6f926d257aaf8bd97f402a1c3fadcdb4316e3
Rapide-S <2453697442@qq.com>
2019-10-10 23:20:23 +0800

    flink-samples

---
+++ flink/src/main/java/com/citi/processfunction/CountWithTimestamp.java
@@ -0,0 +1,14 @@
+package com.citi.processfunction;
+
+/**
+ * @author: Matthew
+ * @date: 2019/10/10 23:03
+ */
+/**
+ * 存储在state中的数据类型
+ */
+public class CountWithTimestamp {
+    public String key;
+    public long count;
+    public long lastModified;
+}
---
+++ flink/src/main/java/com/citi/processfunction/ProcessFunction.java
@@ -0,0 +1,27 @@
+package com.citi.processfunction;
+
+import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.streaming.api.datastream.DataStream;
+
+/**
+ * @author: Matthew
+ * @date: 2019/10/10 23:00
+ * https://cloud.tencent.com/developer/article/1481829
+ */
+public class ProcessFunction{
+    public static void main(String[]args){
+
+
+//        // 数据源
+//        DataStream<Tuple2<String, String>> stream =;
+//
+//// 对KeyedStream应用ProcessFunction
+//        DataStream<Tuple2<String, Long>> result = stream
+//        .keyBy(0)
+//        .process(new CountWithTimeoutFunction());
+
+
+
+        }
+
+}
