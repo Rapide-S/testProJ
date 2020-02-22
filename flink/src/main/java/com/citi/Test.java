bb9894f1d95a96e1e6ee70ca54d7171b3044fe81
Rapide-S <2453697442@qq.com>
2020-02-22 22:34:25 +0800

    log config

--- flink/src/main/java/com/citi/Test.java
+++ flink/src/main/java/com/citi/Test.java
@@ -1,17 +1,27 @@
 package com.citi;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+//import org.slf4j.Logger;
+//import org.slf4j.LoggerFactory;
+
+
+//import org.slf4j.Logger;
+//import org.slf4j.LoggerFactory;
+
+import org.apache.log4j.Logger;
+
+//import java.util.logging.Logger;
 
 /**
  * @author: Matthew
  * @date: 2019/9/15 23:22
  */
 public class Test {
-    private static Logger logger = LoggerFactory.getLogger(Test.class);
+//    private static Logger logger = LoggerFactory.getLogger(Test.class);
+    private static Logger logger = Logger.getLogger(Test.class);
+//    private static Logger logger = Logger.getLogger("Test");
     public static void main(String[] args) {
-        logger.info("hello");
-
+        logger.info("start");
         System.out.println("dada");
+        logger.info("end");
     }
 }
